fa547cffd8a9db3b14d0a6036ae0de1fe2fff62f
sirmatach <32547747+sirmatach@users.noreply.github.com>
2018-01-25 19:35:32 +0000

    Add files via upload

---
+++ Bluetooth.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(0,1);
+
+//String command = "helloWorld";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Yo start");
+  BTSerial.begin(9600);
+
+}
+
+void loop() {
+  //Serial.println("im sending info via bluetooth");
+  //delay(1000);
+  if (Serial.available()){
+    delay(10); // The delay is necessary to get this working!
+    BTSerial.write(Serial.read());
+  }
+}
