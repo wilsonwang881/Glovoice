7dd3811940141d7a21f7c646aa78cc7bfba3bb57
du116 <36479220+du116@users.noreply.github.com>
2018-03-08 19:09:40 +0000

    Add files via upload

---
+++ Glovoice/mongodb.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient()
+db = client.database
+
+def builddb():
+
+ db.words.remove()
+ dictionary = open("dictionary.txt", 'r')
+
+ for line in dictionary:
+       word, num = line.split(" ")
+       num = float(num)
+       db.words.insert_one({"word":word,"float":num})
+
+ dictionary.close()
+
+def word(f):
+    return db.words.find_one({"float": f}, {'_id': 0, "word": 1})['word']
+
+builddb()
+print(word(1.6))
